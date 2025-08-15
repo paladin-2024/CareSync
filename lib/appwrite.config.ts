9415b1caf9d7ecc172493f11c01db56b4681ae40
paladin-2024 <cnzabb@gmail.com>
2025-08-15 22:29:57 +0300

    Add TypeScript declaration file for user and appointment-related types, clean up Appwrite config

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -13,7 +13,6 @@ client
     .setProject(PROJECT_ID!)
     .setKey(API_KEY!)
 
-
 export const databases = new sdk.Databases(client);
 export const storage = new sdk.Storage(client);
 export const users = new sdk.Users(client);
