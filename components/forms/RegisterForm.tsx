89497083cbc288c33614d1e585b0093a61c1cfa9
paladin-2024 <cnzabb@gmail.com>
2025-08-17 19:06:15 +0300

    Extend `RegisterForm` with additional fields and medical information section
    - Added `address` and `occupation` fields.
    - Included `emergencyContactName` and `emergencyContactPhone` fields.
    - Introduced a placeholder section for medical information.
    - Updated placeholder text for birth date and corrected a typo in label class.

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -92,7 +92,7 @@ const RegisterForm=({user}:{user:User})=> {
                         control={form.control}
                         name="birthDate"
                         label="Date of Birth"
-                        placeholder="nzab@gmail.com"
+                        placeholder="Select your birth date"
                         iconSrc="/assets/icons/email.svg"
                         iconAlt="email"
                     />
@@ -110,7 +110,7 @@ const RegisterForm=({user}:{user:User})=> {
                                     {GenderOptions.map((option)=>(
                                         <div key={option} className="radio-group">
                                             <RadioGroupItem value={option} id={option}/>
-                                            <label htmlFor={option} className="cursor-point">
+                                            <label htmlFor={option} className="cursor-pointer">
                                                 {option}
                                             </label>
                                         </div>
@@ -122,12 +122,47 @@ const RegisterForm=({user}:{user:User})=> {
                 </div>
 
                 <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="address"
+                        label="Address"
+                        placeholder="Plot12, Kampala Road, Nakawa, Kampala"
+                    />
+
+                    <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="occupation"
+                        label="Occupation"
+                        placeholder="Software Engineer"
+                    />
 
                 </div>
 
                 <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="emergencyContactName"
+                        label="Energency Contact Name"
+                        placeholder="Guardian's name"
+                    />
 
+                    <CustomFormField
+                        fieldType={FormFieldType.PHONE_INPUT}
+                        control={form.control}
+                        name="emergencyContactPhone"
+                        label="Emergency Contact Phone"
+                        placeholder="+256 700000000"
+                    />
                 </div>
+
+                <section className="space-y-6">
+                    <div className="mb-9 space-y-9">
+                        <h2 className="sub-header">Medical Information</h2>
+                    </div>
+                </section>
 
                 <div className="flex flex-col gap-6 xl:flex-row">
 
