374c809d4f9113ffedeef3ea544f55a582f304d7
paladin-2024 <cnzabb@gmail.com>
2025-08-18 15:33:52 +0300

    Update `Register` layout with responsive adjustments and simplified footer

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -10,8 +10,8 @@ const Register =async ({params:{userId}}:SearchParamProps) => {
     return (
         <div className="flex h-screen max-h-screen">
             {/* TODO:OTP Verification or  PasskeyModel*/ }
-            <section className="remove-scrollbar container my-auto">
-                <div className="sub-container max-w-[496px]">
+            <section className="remove-scrollbar container">
+                <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
                     <Image
                         src="/assets/icons/logo-full.svg"
                         height={1000}
@@ -21,15 +21,10 @@ const Register =async ({params:{userId}}:SearchParamProps) => {
                     />
 
                     <RegisterForm user = {user}/>
+                    <p className="copyright py-12">
+                        © 2025 CarePulse
+                    </p>
 
-                    <div className="text-14-regular mt-20 flex justify-between">
-                        <p className="justify-items-end text-dark-600 xl:text-left">
-                            © 2025 CarePulse
-                        </p>
-                        <Link href="/?admin=true" className="text-green-500">
-                            Admin
-                        </Link>
-                    </div>
                 </div>
             </section>
 
