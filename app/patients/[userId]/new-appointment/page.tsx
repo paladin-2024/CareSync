4fe316d5f8861782bbcf4f1779d2aa78d1be0f46
paladin-2024 <cnzabb@gmail.com>
2025-08-26 22:53:30 +0300

    Refactor `NewAppointment` page to use arrow function syntax.

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import AppointmentForm from "@/components/forms/AppointmentForm";
 import {getPatient} from "@/lib/actions/patient.actions";
 
-export default async function NewAppointment({params:{userId}}:SearchParamProps) {
+const  NewAppointment= async({params:{userId}}:SearchParamProps)=> {
     const patient=await getPatient(userId);
     return (
         <div className="flex h-screen max-h-screen">
@@ -38,3 +38,4 @@ export default async function NewAppointment({params:{userId}}:SearchParamProps)
         </div>
     );
 }
+export default NewAppointment;
