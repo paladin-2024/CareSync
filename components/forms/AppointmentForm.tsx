1b42965cbc65da1a465265d325c9a9874e8fecd6
paladin-2024 <cnzabb@gmail.com>
2025-08-27 21:04:38 +0300

    Fix incorrect property in `router.push` URL in `AppointmentForm`.

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -73,7 +73,7 @@ const AppointmentForm=({
                 console.log(appointment)
                 if (appointment) {
                     form.reset();
-                    router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.id}`)
+                    router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`)
                 }
             }
         } catch (error){
