2962c849b18451aa59ff7069666a4dbeb8b68a1a
paladin-2024 <cnzabb@gmail.com>
2025-09-26 14:35:42 +0300

    Refactor `AppointmentForm` to integrate `react-hook-form`, add validation schema, and update UI components for better user experience.

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -1,8 +1,108 @@
-import React from 'react'
+"use client"
 
-const AppointmentForm = () => {
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {Form} from "@/components/ui/form"
+import CustomFormField from "@/components/CustomFormField";
+import SubmitButton from "@/components/SubmitButton";
+import {useState} from "react";
+import {useRouter} from "next/navigation"
+import {UserFormValidation} from "@/lib/validation";
+import {createUser} from "@/lib/actions/patient.actions";
+import {FormFieldType} from "@/components/forms/PatientForm";
+import {Doctors} from "@/constants";
+import {SelectItem} from "@/components/ui/select";
+import Image from "next/image";
+
+
+const AppointmentForm=()=> {
+
+    const router = useRouter();
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
+        defaultValues: {
+            name: "",
+            email:"",
+            phone:"",
+        },
+    })
+
+    async function onSubmit({name,email,phone}: z.infer<typeof UserFormValidation>) {
+        setIsLoading(true);
+
+        try {
+            const userData = {name, email, phone};
+            const user = await createUser(userData);
+            if (user) router.push(`/patients/${user.$id}/register`)
+        } catch (error){
+            console.log(error)
+        }
+    }
     return (
-        <div>AppointmentForm</div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+                <section className="mb-12 space-y-4">
+                    <h1 className="header">Hi there 👋</h1>
+                    <p className="text-dark-700">Schedule your first appointment.</p>
+                </section>
+
+                <CustomFormField
+                    fieldType={FormFieldType.SELECT}
+                    control={form.control}
+                    name="primaryPhysician"
+                    label="Primary Physician"
+                    placeholder="Select a Physician">
+                    {Doctors.map((doctor)=>(
+                        <SelectItem key={doctor.name} value={doctor.name}>
+                            <div className="flex cursor-pointer items-center gap-2">
+                                <Image
+                                    src={doctor.image}
+                                    width={32}
+                                    height={32}
+                                    alt={doctor.name}
+                                    className="rounded-full border border-dark-500"
+                                />
+                                <p>{doctor.name}</p>
+                            </div>
+                        </SelectItem>
+                    ))}
+                </CustomFormField>
+
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        fieldType={FormFieldType.TEXTAREA}
+                        control={form.control}
+                        name="allergies"
+                        label="Allergies (if any)"
+                        placeholder="Peanuts, Penicillin, Eggs, Lactose, etc."
+                    />
+
+                    <CustomFormField
+                        fieldType={FormFieldType.TEXTAREA}
+                        control={form.control}
+                        name="currentMedications"
+                        label="Current Medications (if any)"
+                        placeholder="Ibuprofen 200mg, Paracetamol 500mg, Antacids 250mg, etc."
+                    />
+                </div>
+
+                <CustomFormField
+                    fieldType={FormFieldType.DATE_PICKER}
+                    control={form.control}
+                    name="birthDate"
+                    label="Date of Birth"
+                    placeholder="Select your birth date"
+                />
+
+                <SubmitButton isLoading={isLoading}>
+                    Get Started
+                </SubmitButton>
+            </form>
+        </Form>
     )
 }
 export default AppointmentForm
